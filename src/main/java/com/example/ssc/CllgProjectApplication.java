ce05f979e15171c21f69a366bba633bc445d6a8e
Siddhesh <shirkesiddhesh03@gmail.com>
2023-06-15 15:59:54 +0530

    setup

---
+++ src/main/java/com/example/ssc/CllgProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CllgProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CllgProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ssc/CllgProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CllgProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
